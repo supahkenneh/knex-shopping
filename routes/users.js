1eb806b390ff45d87f09cc831b261feac588f329
Kenny Chung <kcmac@Kennys-MacBook-Pro.local>
2018-08-02 16:52:17 -1000

    user login

--- routes/users.js
+++ routes/users.js
@@ -14,4 +14,25 @@ router.get('/:user_id', (req, res) => {
   .catch(err => console.log(err))
 });
 
+router.post('/login', (req, res) => {
+  const data = req.body;
+  db.raw('SELECT email, password FROM users WHERE email = ?', [data.email])
+  .then(user => {
+    if(!user || !user.rowCount) {
+      return res.status(404).send(' { "message" : "User not found" }');
+    }
+    return user;
+  })
+  .then(user => {
+    if(user.rows[0].password !== data.password) {
+      return res.status(200).send(' { "message" : Incorrect password }');
+    }
+    return user;
+  })
+  .then(user => {
+    res.send(user.rows);
+  })
+  .catch(err => console.log(err));
+});
+
 module.exports = router;
--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app =  express();
+const bodyParser = require('body-parser');
 const PORT = process.env.port || 3000;
 const cart = require('./routes/cart');
 const products = require('./routes/products');
 const users = require('./routes/users');
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/cart', cart);
 app.use('/products', products);
@@ -13,5 +16,5 @@ app.get('/', (req, res) => {
   res.send('hello world');
 });
 
-app.listen(PORT, () => console.log('Connected to server'));
+app.listen(PORT, () => console.log(`Connected to server on port: ${PORT}`));
 
