dc1e6aa2bcf8064c7fe31b1832b8f26f8c816091
Kenny Chung <kcmac@Kennys-MacBook-Pro.local>
2018-08-02 16:05:57 -1000

    create tables, knex initiated

---
+++ db/setup_db.sql
@@ -0,0 +1,36 @@
+\c postgres;
+
+DROP DATABASE IF EXISTS knex_shopping;
+DROP USER IF EXISTS knex_shopping_user;
+
+CREATE USER knex_shopping_user WITH PASSWORD 'password';
+CREATE DATABASE knex_shopping WITH OWNER knex_shopping_user;
+
+\c knex_shopping knex_shopping_user;
+
+CREATE TABLE users (
+  id serial NOT NULL PRIMARY KEY,
+  email varchar(255) NOT NULL,
+  password varchar(255) NOT NULL,
+  created_at TIMESTAMP NOT NULL,
+  updated_at TIMESTAMP NOT NULL
+);
+
+CREATE TABLE products (
+  id serial NOT NULL PRIMARY KEY,
+  title varchar(255) NOT NULL,
+  description text,
+  inventory integer,
+  price decimal(8, 2),
+  created_at TIMESTAMP NOT NULL,
+  updated_at TIMESTAMP NOT NULL
+);
+
+CREATE TABLE cart (
+  id serial NOT NULL PRIMARY KEY,
+  user_id integer REFERENCES users(id), 
+  products_id integer REFERENCES products(id),
+  created_at TIMESTAMP NOT NULL,
+  updated_at TIMESTAMP NOT NULL
+);
+
---
+++ knexfile.js
@@ -0,0 +1,54 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'postgresql',
+    connection: {
+      database: 'knex_shopping',
+      user: 'knex_shopping_user',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    },
+    debug: false
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user: 'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user: 'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
