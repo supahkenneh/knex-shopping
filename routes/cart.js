cb8067eed6abacf9680f837cc8483aeb4a063af0
Kenny Chung <kcmac@Kennys-MacBook-Pro.local>
2018-08-02 20:38:53 -1000

    post cart

--- routes/cart.js
+++ routes/cart.js
@@ -17,4 +17,24 @@ router.get('/:user_id', (req, res) => {
   .catch(err => console.log(err));
 });
 
+router.post('/:user_id/:product_id', (req, res) => {
+  const userId = req.params.user_id;
+  const productId = req.params.product_id;
+
+  db.raw('SELECT user_id, products_id FROM cart WHERE user_id = ? AND products_id = ?', [userId, productId])
+  .then(result => {
+    if (!result || !result.rowCount) {
+      return res.status(404).json({ 'success' : 'failed' });
+    }
+    return result;
+  })
+  .then(result => {
+    return db.raw('INSERT INTO cart (user_id, products_id) VALUES (? , ?)', [userId, productId])
+  })
+  .then(result => {
+    res.json({'success' : 'true'})
+  })
+  .catch(err => console.log(err));
+});
+
 module.exports = router;
