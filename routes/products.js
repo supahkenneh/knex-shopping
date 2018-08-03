1b5ae6165b4e984c71e26a220da1246513921dd8
Kenny Chung <kcmac@Kennys-MacBook-Pro.local>
2018-08-02 19:54:20 -1000

    post new item

--- routes/products.js
+++ routes/products.js
@@ -4,31 +4,44 @@ const db = require('../db/knex');
 
 router.get('/', (req, res) => {
   db.raw('SELECT * FROM products')
-  .then(result => {
-    if(!result.rows) {
-      res.status(404).json({'message': 'There are no products'})
-    }
-    return result;
-  })
-  .then(result => {
-    res.json(result.rows);
-  })
-  .catch(err => console.log(err));
+    .then(result => {
+      if (!result.rows) {
+        res.status(404).json({ 'message': 'There are no products' })
+      }
+      return result;
+    })
+    .then(result => {
+      res.json(result.rows);
+    })
+    .catch(err => console.log(err));
 });
 
 router.get('/:product_id', (req, res) => {
   const id = req.params.product_id;
   db.raw('SELECT * FROM products WHERE id = ?', [id])
-  .then(result => {
-    if (!result || !result.rowCount) {
-      return res.status(404).json({'Message': "Product not found"})
-    }
-    return result;
-  })
-  .then(result => {
-    res.json(result.rows[0]);
-  })
-  .catch(err => console.log(err));
+    .then(result => {
+      if (!result || !result.rowCount) {
+        return res.status(404).json({ 'Message': "Product not found" })
+      }
+      return result;
+    })
+    .then(result => {
+      res.json(result.rows[0]);
+    })
+    .catch(err => console.log(err));
+});
+
+router.post('/new', (req, res) => {
+  const data = req.body;
+  if (!data.title || !data.description || !data.inventory || !data.price) {
+    return res.status(400).json({ 'message': "Must fill all product fields" });
+  } else {
+    return db.raw('INSERT INTO products (title, description, inventory, price) VALUES (?, ?, ?, ?) RETURNING *', [data.title, data.description, data.inventory, data.price])
+    .then(result => {
+      res.json(result.rows[0]);
+    })
+    .catch(err => console.log(err));
+  }
 })
 
 module.exports = router;
