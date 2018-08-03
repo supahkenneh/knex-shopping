7136f6062910b64d59a75b3b358707dd0c6ca152
Kenny Chung <kcmac@Kennys-MacBook-Pro.local>
2018-08-02 16:35:53 -1000

    made dbreset script, prepopulate users, get/userid working

--- db/setup_db.sql
+++ db/setup_db.sql
@@ -12,8 +12,8 @@ CREATE TABLE users (
   id serial NOT NULL PRIMARY KEY,
   email varchar(255) NOT NULL,
   password varchar(255) NOT NULL,
-  created_at TIMESTAMP NOT NULL,
-  updated_at TIMESTAMP NOT NULL
+  created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+  updated_at TIMESTAMP NOT NULL DEFAULT NOW()
 );
 
 CREATE TABLE products (
@@ -22,15 +22,24 @@ CREATE TABLE products (
   description text,
   inventory integer,
   price decimal(8, 2),
-  created_at TIMESTAMP NOT NULL,
-  updated_at TIMESTAMP NOT NULL
+  created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+  updated_at TIMESTAMP NOT NULL DEFAULT NOW()
 );
 
 CREATE TABLE cart (
   id serial NOT NULL PRIMARY KEY,
   user_id integer REFERENCES users(id), 
   products_id integer REFERENCES products(id),
-  created_at TIMESTAMP NOT NULL,
-  updated_at TIMESTAMP NOT NULL
+  created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+  updated_at TIMESTAMP NOT NULL DEFAULT NOW()
 );
+
+INSERT INTO users (email, password, created_at, updated_at) 
+VALUES (
+  'whatever@gmail.com',
+  'password',
+  default,
+  default
+)
+RETURNING *;
 
--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const router = express.Router();
+const db = require('../db/knex');
 
-router.get('/', (req, res) => {
-  res.send('you got users');
+router.get('/:user_id', (req, res) => {
+  const id = req.params.user_id;
+  db.raw('SELECT email FROM users WHERE id = ?', [id])
+  .then(user => {
+    if(!user || !user.rowCount) {
+      res.status(400).send(`{ message: 'User not found'}`);
+    }
+    res.json(user.rows[0]);
+  })
+  .catch(err => console.log(err))
 });
 
 module.exports = router;
